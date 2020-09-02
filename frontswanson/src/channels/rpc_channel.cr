8ed8b62982fb9b9665b6a21f4ffd467dc8ad8402
Juozas Gaigalas <juozasgaigalas@gmail.com>
2020-09-02 18:35:00 -0400

    nats proxy on demo.nats.io:swanson-rpc

--- frontswanson/src/channels/rpc_channel.cr
+++ frontswanson/src/channels/rpc_channel.cr
@@ -5,15 +5,10 @@ require "kilt/slang"
 class RpcChannel < Amber::WebSockets::Channel
   def handle_joined(client_socket, message)
     puts "WebSocket RpcChannel browser joined"
-    # RpcSocket.broadcast("message", "rpc", "message_new", {"handle" => "here"})
-    SwansonProxy.start_grpc_http2_server
+    SwansonProxy.start_nats_server
   end
 
   def handle_message(client_socket, message)
-    # puts("-------------")
-    # puts(message.class)
-    # puts(message)
-    # dispatch(client_socket, message)
     puts "WebSocket RpcChannel browser sent '#{message}'"
   end
 
--- frontswanson/src/models/swanson_proxy.cr
+++ frontswanson/src/models/swanson_proxy.cr
@@ -1,43 +1,26 @@
-
-require "../protobufs/swanson.pb"
-require "../protobufs/swanson_services.pb"
 require "nats"
-require "grpc/http2"
-
-class SwansonWorldHandler < SwansonWorld
-  def create(swanson : Swanson) : Result
-    SwansonProxy.websocket_rpc_spawn(swanson.body.to_s)
-    Result.new(result: "a faily decent good result right here")
-  end
-end
 
 class SwansonProxy
   def self.start_nats_server
-    # grpc = GRPC::Server.new
-    # grpc << SwansonWorldHandler.new
+    nc = NATS::Connection.new("demo.nats.io")
+    nc.subscribe("swanson-rpc") { |msg|SwansonProxy.websocket_rpc_send("NATS: #{msg}")}
+    nc.publish("swanson-rpc", "swanson-rpc is ready")
 
-    # server = HTTP2::ClearTextServer.new([grpc]) # TLS isn't supported yet
-    # spawn do
-    # end
-    # puts("gRPC HTTP/2 listening on 0.0.0.0:5000")
-    # server.listen "0.0.0.0", 50000
-    # puts("done listening")
 
     # # send out initial message to websocket
-    # spawn do
-    #   sleep 0.1
-    #   SwansonProxy.websocket_rpc_spawn("yooooo--- swansons await grpc messages on PORT 50000")
-    # end
+    spawn do
+      sleep 0.1
+      SwansonProxy.websocket_rpc_send("swanson websockets is ready")
+    end
   end
     
-  def self.websocket_rpc_spawn(body : String)
+  def self.websocket_rpc_send(body : String)
     puts "WebSocket RpcChannel broadcasting to joined browsers '#{body}'"
     RpcSocket.broadcast("message", "rpc", "message_new", {
     "velx" => 2.3 * (Random.new.next_bool ? 1 : -1),
     "vely" => 1.1 * (Random.new.next_bool ? 1 : -1),
     "html" => Kilt.render("src/views/home/_entity.slang"),
     "time" => Time.utc.to_s
-  })
-end
-  
+    })
+  end
 end
